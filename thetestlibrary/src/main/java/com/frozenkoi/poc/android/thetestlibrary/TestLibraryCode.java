615fa8b41c249b736543bf2f3576bae5cf1465fb
FrozenKoi <frozenkoi@gmail.com>
2018-03-14 02:56:55 -0600

    Add functionality and configure publish to local maven

---
+++ thetestlibrary/src/main/java/com/frozenkoi/poc/android/thetestlibrary/TestLibraryCode.java
@@ -0,0 +1,11 @@
+package com.frozenkoi.poc.android.thetestlibrary;
+
+/**
+ * Created by Daniel on 3/8/2018.
+ */
+
+public class TestLibraryCode {
+    public static final boolean isThisLibraryForTesting() {
+        return true;
+    }
+}
